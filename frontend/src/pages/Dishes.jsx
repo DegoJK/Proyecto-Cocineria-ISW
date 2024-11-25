3bda66e2eb4402bc95c64c8518a8f684aed722e6
Diego Aguilera <113396231+DegoJK@users.noreply.github.com>
2024-11-25 00:06:32 -0300

    actualizacion

--- frontend/src/pages/Dishes.jsx
+++ frontend/src/pages/Dishes.jsx
@@ -1,14 +1,64 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { getDishes, deleteDish } from "../services/dishes.service.js";
 import '@styles/dishes.css';
 
-const Dishes = () => {
-  return (
-    <div className="dishes-container">
-        <div className="dishes-title">
-          <h1>Platos</h1>
-        </div>
-    </div>
-  );
-};
+export default function Dishes() {
+    const [dishes, setDishes] = useState([]);
 
-export default Dishes;
+    const fetchDishes = async () => {
+        try {
+            const response = await getDishes();
+            setDishes(response);
+        }catch (error) {
+            console.error('Error: ', error);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await deleteDish(id);
+            console.log("Platillo eliminado",response);
+            await fetchDishes();
+        } catch (error) {
+            console.error('Error: ',error);
+        }
+
+    }
+
+    useEffect(() => {
+        fetchDishes();
+    }, []);
+
+    return (
+        <div>
+            <div className="dish-header">
+            <h1>Pagina de Platillos</h1>
+            </div>
+            {dishes?.length > 0 ? (
+                <ul>
+                    <div className="dish-list">
+                    {dishes.map((dish) => (
+
+                        <div key = {dish.id}>
+                            <li>
+                                <div className="dish-container">
+                                    <p>Platillo: {dish.nombre}</p>
+                                    <p>Descripcion: {dish.descripcion} </p>
+                                    <p>Precio: {dish.precio} </p>
+                                    <p>Estado: {dish.estado} </p>
+                                    <img src={dish.imagen} alt="imagen" />
+                                    
+                                    <button onClick={() => handleDelete(dish.id)}>Eliminar</button>
+                                </div>
+                            </li>
+                        </div>
+                    ))}
+                    </div>
+                </ul>
+            ) : (
+                <p>No hay platillos</p>
+            )}
+        </div>
+    )
+
+}
